f628b58f2ca4471c1d67dbfc8e825295d14b1555
Dmitry <yarich1@yandex.ru>
2024-05-12 15:09:26 +0300

    fixing fetch

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
-const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
-
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
@@ -13,10 +12,17 @@ module.exports = {
   target,
   devtool,
   devServer: {
-    open: true,
-    hot: true,
+    static: {
+      directory: path.resolve(__dirname, './dist/'), // Указываем путь к каталогу, из которого будут обслуживаться файлы
+    },
+    compress: true, // Включаем сжатие файлов
+    port: 8080, // Указываем порт для локального сервера
+    open: true, // Автоматически открывать браузер при запуске сервераоматически открывать браузер при запуске сервера
+
   },
-  entry: path.resolve("@babel/polyfill", __dirname, 'coffee-house', 'src', 'index.js'),
+  entry: {
+    main: ['@babel/polyfill', './coffee-house/src/index.js']
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
@@ -26,16 +32,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
-      template: path.resolve(__dirname, 'coffee-house', 'src', 'pages', 'home', 'index.html'),
-      // template: '../coffe-house/coffee-house/src/pages/home/index.html',
+      template: './coffee-house/src/pages/home/index.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'menu.html',
-      template: path.resolve(__dirname, 'coffee-house', 'src', 'pages', 'menu', 'menu.html'),
-      // template: '../coffe-house/coffee-house/src/pages/menu/menu.html',
+      template: './coffee-house/src/pages/menu/menu.html',
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contanthash].css',
+      filename: '[name].[contenthash].css',
     })
   ],
   module: {   
@@ -110,4 +114,118 @@ module.exports = {
         },
     ],
   },
-};
+}; 
+
+// const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
+
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// const mode = process.env.NODE_ENV || 'development';
+// const devMode = mode === 'development';
+// const target = devMode ? 'web' : 'browserslist'; 
+// const devtool = devMode ? 'source-map' : undefined;
+
+// module.exports = {
+//   mode,
+//   target,
+//   devtool,
+//   devServer: {
+//     open: true,
+//     hot: true,
+//   },
+//   entry: path.resolve("@babel/polyfill", __dirname, 'coffee-house', 'src', 'index.js'),
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     clean: true,
+//     filename: '[name].[contenthash].js',
+//     assetModuleFilename: 'assets/[name][ext]'
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       filename: "index.html",
+//       template: path.resolve(__dirname, 'coffee-house', 'src', 'pages', 'home', 'index.html'),
+//       // template: '../coffe-house/coffee-house/src/pages/home/index.html',
+//     }),
+//     new HtmlWebpackPlugin({
+//       filename: 'menu.html',
+//       template: path.resolve(__dirname, 'coffee-house', 'src', 'pages', 'menu', 'menu.html'),
+//       // template: '../coffe-house/coffee-house/src/pages/menu/menu.html',
+//     }),
+//     new MiniCssExtractPlugin({
+//       filename: '[name].[contanthash].css',
+//     })
+//   ],
+//   module: {   
+//     rules: [
+//         {
+//             test: /\.html$/i,
+//             loader: "html-loader",
+//         },
+//         {
+//             test: /\.(c|sa|sc)ss$/i,
+//             use: [
+//                 devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+//                  "css-loader",
+//                  {
+//                     loader: 'postcss-loader',
+//                     options: {
+//                         postcssOptions: {
+//                             plugins: [require('postcss-preset-env')],
+//                         }  
+//                     }
+//                  },
+//                  "sass-loader"
+//             ],
+//         },
+//         {
+//             test:/\.woff2?$/i,
+//             type: 'asset/resource',
+//             generator: {
+//                 filename: 'fonts/[name][ext]'
+//             }
+//         },
+//             {
+//             test:/\.(jpe?g|png|webp|gif|svg)$/i,
+//             use: [
+//                 {
+//                 loader: 'image-webpack-loader',
+//                 options: {
+//                     mozjpeg: {
+//                       progressive: true,
+//                     },
+//                     // optipng.enabled: false will disable optipng
+//                     optipng: {
+//                       enabled: false,
+//                     },
+//                     pngquant: {
+//                       quality: [0.65, 0.90],
+//                       speed: 4
+//                     },
+//                     gifsicle: {
+//                       interlaced: false,
+//                     },
+//                     // the webp option will enable WEBP
+//                     webp: {
+//                       quality: 75
+//                     }
+//                 }
+//                 }
+//             ],
+//             type: 'asset/resource',
+//         },
+//         {
+//             test: /\.(?:js|mjs|cjs)$/i,
+//             exclude: /node_modules/,
+//             use: {
+//               loader: 'babel-loader',
+//               options: {
+//                 presets: [
+//                   ['@babel/preset-env', { targets: "defaults" }]
+//                 ]
+//               }
+//             }
+//         },
+//     ],
+//   },
+// };
