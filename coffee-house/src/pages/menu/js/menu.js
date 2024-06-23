328a212ef505e564dc782e00e3ea023cb95677ec
Dmitry <yarich1@yandex.ru>
2024-06-23 10:52:31 +0300

    feat: continue menu js functional

--- coffee-house/src/pages/menu/js/menu.js
+++ coffee-house/src/pages/menu/js/menu.js
@@ -2,9 +2,20 @@ console.log("hello menu world");
 
 let menuTabItems = document.querySelectorAll(".menu__tab-item");
 
+let menuTabItem = document.querySelector('.menu__icon-text');
+
 menuTabItems.forEach((item) => {
     item.addEventListener('click', function (e) {
-        console.log(item);
+        if (item.children[1].innerText === 'Coffee') {
+            console.log('это кофе');
+        } else if 
+            (item.children[1].innerText === 'Tea') {
+               console.log('это чай');
+            }
+         else if (item.children[1].innerText === 'Dessert') {
+                console.log('это десерт');
+             }
+        // console.log(item.children[1].innerText);
     })
 }
 )
